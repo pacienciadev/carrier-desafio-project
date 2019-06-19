4d5c200de61f0f9b7615ab9d89b2d1a39d206592
Rafael Eduardo <rafapaci@hotmail.com>
2019-06-19 01:47:41 -0300

    Ajustes Finais

--- resources/views/relatorios/index.blade.php
+++ resources/views/relatorios/index.blade.php
@@ -35,22 +35,29 @@
         </li>
     </ul>
     @php
+    if($quantidade_total !== 0){
         //Regra de 3 para o gráfico masculino
         $porcentagem_masc = (100*$funcionarios_masculino)/$quantidade_total;
         //Regra de 3 para o gráfico Feminino
         $porcentagem_fem = (100*$funcionarios_feminino)/$quantidade_total;
+    }else {
+        $porcentagem_masc = 0;
+        $porcentagem_fem = 0;
+    }   
     @endphp
     <ul class="list-group col-md-8">
         <li class="list-group-item d-flex justify-content-between align-items-center">
             <strong>Gráfico</strong> 
         </li>
-        <li class="list-group-item d-block">
-            <div class="progress" style="height: 24px">
-                <div class="progress-bar bg-success" role="progressbar" style="width: {{ $porcentagem_masc }}%" aria-valuenow="{{ $porcentagem_masc }}" aria-valuemin="0" aria-valuemax="100"></div>
+        <li class="list-group-item" style="height: 50px">
+            <small><strong>{{ $porcentagem_masc }}% - Masculino</strong></small>
+            <div class="progress" style="height: 4.5px">
+               <div class="progress-bar bg-success" role="progressbar" style="width: {{ $porcentagem_masc }}%" aria-valuenow="{{ $porcentagem_masc }}" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </li>
-        <li class="list-group-item">
-            <div class="progress" style="height: 24px">
+        <li class="list-group-item" style="height: 50px">
+            <small><strong>{{ $porcentagem_fem }}% - Feminino</strong></small>
+            <div class="progress" style="height: 5px">
                 <div class="progress-bar bg-danger" role="progressbar" style="width: {{ $porcentagem_fem }}%" aria-valuenow="{{ $porcentagem_fem }}" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </li>
